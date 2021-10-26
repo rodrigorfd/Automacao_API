76f6683f70ea0ea77174c1499da89f22eb0d25ee
Rodrigo  F. Dutra <rodrigorfdutra@gmail.com>
2021-10-26 20:19:07 -0300

    Automação-API

--- src/test/java/br/com/restassuredapitesting/tests/booking/tests/PostBookingTest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/tests/PostBookingTest.java
@@ -12,6 +12,7 @@ import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import static org.hamcrest.Matchers.hasValue;
 import static org.hamcrest.Matchers.notNullValue;
 
 @Feature("Feature - Criação de Reservas ")
@@ -50,10 +51,10 @@ public class PostBookingTest extends BaseTest {
     @Category({AllTests.class, AcceptanceTests.class, E2eTests.class})
     @DisplayName("Criar uma reserva com parâmetros a mais")
     public void criarReservaComParametrosAMais(){
-        postBookingRequest.criaUmaReservaComParametrosAMais()
+        postBookingRequest.262626()
                 .then()
                 .statusCode(200)
-                .body("bookingid",notNullValue());
+                .log().all();
     }
 
     @Test
